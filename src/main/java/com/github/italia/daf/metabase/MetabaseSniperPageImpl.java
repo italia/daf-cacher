6a1972e95a44b763efc247557ecacf94d072ce35
Francesco <francesco.laurita@gmail.com>
2017-11-11 07:04:23 -0800

    Allow maps to fully load

--- src/main/java/com/github/italia/daf/metabase/MetabaseSniperPageImpl.java
+++ src/main/java/com/github/italia/daf/metabase/MetabaseSniperPageImpl.java
@@ -2,13 +2,23 @@ package com.github.italia.daf.metabase;
 
 import com.github.italia.daf.sniper.Page;
 import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
 
 public class MetabaseSniperPageImpl implements Page {
     @Override
     public void visit(WebDriver driver, String url) {
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get(url);
+        (new WebDriverWait(driver, 20)).until((ExpectedCondition<Boolean>) d -> {
+            final String status = (String)((JavascriptExecutor)d).executeScript("return document.readyState");
+            System.out.println("Status: " + status);
+            return status.equals("complete");
+        });
+
         try {
             final WebElement spinner = driver.findElement(By.className("LoadingSpinner"));
             if (spinner.isDisplayed()) {
@@ -26,6 +36,11 @@ public class MetabaseSniperPageImpl implements Page {
                 .until(
                         ExpectedConditions.visibilityOf(
                                 driver.findElement(By.name("downarrow"))));
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
     }
 
     @Override
